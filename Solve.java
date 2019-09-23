21095dcf7b6f3fbebfde83575bfe4852d68f7f92
Calvin Vuong <calvinvuong425@gmail.com>
2019-09-23 16:48:34 -0400

    Wrote code for local beam search; not debugged.

--- Solve.java
+++ Solve.java
@@ -34,9 +34,15 @@ public class Solve {
 	    }
 	    
 	    else if ( commandLine.toLowerCase().startsWith("solve a-star") ) {
-		String heuristic = "h2";
+		String heuristic = "h2"; // hardcoded for now
 		board.evaluate(heuristic);
 		int numMoves = solveAStar(board, heuristic);
+		System.out.println(numMoves);
+	    }
+
+	    else if ( commandLine.toLowerCase().startsWith("solve beam") ) {
+		board.evaluate("h2");
+		int numMoves = solveLocalBeamSearch(board, 15); // k hardcoded for now
 		System.out.println(numMoves);
 	    }
 	    
@@ -60,7 +66,7 @@ public class Solve {
 
 	// start loop
 	while ( ! frontier.isEmpty() ) {
-	    State currentState = frontier.poll();
+	    State currentState = frontier.poll(); // remove from queue
 	    // reached goal
 	    if ( currentState.isGoal() ) {
 		printMoves(currentState, currentState.getPath());
@@ -69,7 +75,7 @@ public class Solve {
 	    // if currentState has already been explored fully, don't bother looking at children
 	    if ( !explored.contains(currentState) ) {
 		explored.add(currentState);
-		
+   
 		// generate children state from set of legal moves
 		boolean[] legal = currentState.legalMoves();
 		for ( int i = 0; i < legal.length; i++ ) {
@@ -110,6 +116,95 @@ public class Solve {
 	// frontier is empty at end of while loop
 	// no solution
 	System.out.println("No solution found.");
+	return -1;
+    }
+
+
+    // Solves the 8-puzzle using a local beam search that keeps track of k states
+    // Returns the number of moves to reach goal, -1 if no solution found
+    // Prints out the steps to reach goal
+    // Starts from input startState
+    // Uses heuristic h2
+    // TODO: Implement maxnodes
+    public static int solveLocalBeamSearch(State startState, int k) {
+	// initialize data structures
+	PriorityQueue<State> beam = new PriorityQueue<State>();
+	HashSet<State> explored = new HashSet<State>();
+	// set of all children generated from states in the beam
+	// implemented as a priority queue, so we can poll() k times to get the k best children
+	PriorityQueue<State> childSet = new PriorityQueue<State>();
+	int counter = 0; // counts from 0 to k
+	
+	beam.add(startState);
+
+	// start loop
+	// terminate search when the beam is empty after children are added onto beam
+	while ( ! beam.isEmpty() ) {
+	    // reset childSet and counter
+	    childSet.clear();
+	    counter = 0;
+	    
+	    // generate children state for each state currently in the beam
+	    while ( ! beam.isEmpty() ) {
+		State currentState = beam.poll(); // remove from queue
+		// check if reached goal
+		if ( currentState.isGoal() ) {
+		    printMoves(currentState, currentState.getPath());
+		    return currentState.getPathCost();
+		}
+		
+		explored.add(currentState);
+		// generate all children of currentState and add to beam
+		// get legal moves
+		boolean[] legal = currentState.legalMoves();
+		for ( int i = 0; i < legal.length; i++ ) {
+		    if ( legal[i] == true ) {
+			State childState = currentState.duplicate();
+			// perform move based on what is legal
+			if ( i == 0 ) {
+			    childState.moveUp();
+			    childState.addPathMove("up");
+			}
+			else if ( i == 1 ) {
+			    childState.moveDown();
+			    childState.addPathMove("down");
+			}
+			else if ( i == 2 ) {
+			    childState.moveLeft();
+			    childState.addPathMove("left");
+			}
+			else if ( i == 3 ) {
+			    childState.moveRight();
+			    childState.addPathMove("right");
+		    }
+			// increment path cost
+			childState.incrementPathCost();
+			// perform evaluation function on child using h2 as heuristic
+			childState.evaluate("h2");
+
+			// add childState to childSet if not already explored
+			if ( !explored.contains(childState) )
+			    childSet.add(childState);
+		    } // close if
+		}  // close generating and adding children for currentState
+	    } // close generating and adding children for all states in beam
+
+	    // reset beam
+	    beam.clear();
+	    // fill beam with k best states from childSet
+	    while ( !childSet.isEmpty() && counter < k ) {
+		// get the best child state from childSet and remove it
+		State bestChild = childSet.poll();
+		// do not add child to beam if already explored
+		if ( !explored.contains(bestChild) ) {
+		    beam.add(bestChild);
+		    counter += 1;
+		}
+	    }
+	}
+
+	// goal not found
+	System.out.println("Goal not found.");
 	return -1;
     }
 
